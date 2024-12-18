b40eb1eefcc610d3335da91bae19b9804e944c90
Hemanthk408 <g.hemanth0408@gmail.com>
2024-12-18 21:37:29 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
 
-df=pd.read_csv('')
+df=pd.read_csv('top_600_youtube_videos_2023.csv')
+df
 
+#for identifi the channal name are unquie 
+df=df.unquie()
 
 from google.cloud import translate_v2 as translate
 
